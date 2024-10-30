21c21727f4c83b3e1b02d4fc359eb4dba3be4fee
sarath <sarath@aicnet.in>
2024-10-30 18:04:49 +0530

    lf log sheet changes

--- src/app/demo/components/logsheets/lf-logsheet/lf-logsheet.component.ts
+++ src/app/demo/components/logsheets/lf-logsheet/lf-logsheet.component.ts
@@ -18,7 +18,8 @@ export class LfLogsheetComponent implements OnInit{
     }
   }
 
-  logSheetList: any[] = [];
+  logSheetList1: any[] = [];
+  logSheetList2: any[] = [];
   selectedData: any;
   steelGradeList: any[]=[{id:1,name:"EN8D"},{id:2,name:"C45R"},{id:3,name:"SAE1040"},{id:4,name:"45C8"},{id:5,name:"EN1A"},{id:6,name:"51CrV4"}]
   shift: any[]=[{id:1,name:'A'},{id:2,name:'B'},{id:3,name:'C'}];
@@ -114,19 +115,88 @@ export class LfLogsheetComponent implements OnInit{
       // const grade = this.steelGradeList.find((item: any) => item.id === rowData.steelGradeId);
       // if (grade) { rowData.gradeName = grade.name; }
 
-      this.logSheetList.push(rowData);
+      this.logSheetList1.push(rowData);
+    }
+  }
+
+  addNewItem_to_List2() {
+    this.submitted = true;
+    if (this.validateForm()) {
+      let rowData = JSON.parse(JSON.stringify(this.dialogData));
+      if(!rowData.date){
+        rowData.date = new Date();
+      }
+      if(!rowData.time){
+        rowData.time = new Date();
+      }
+      this.dialogData = {
+        date:new Date(),
+        heatNo: '',
+        shiftId: 0,
+        steelGradeId: 0,
+        liqTemp: 0,
+        gradeTypeId: 0,
+        tas:'',
+        weight: 0,
+        freeBoard:0,
+        size:0,
+        time: new Date(),
+        ladleTemp:0,
+        arcingTime:0,
+        argonFlow:0,
+        arConsumption:0,
+        carNo:0,
+        lrfProcessTime:0
+      }
+
+      const shift = this.shift.find((item: any) => item.id == rowData.shiftId);
+      if(shift){
+        rowData.shiftName = shift.name;
+      }
+      const grade = this.steelGradeList.find((item: any) => item.id == rowData.steelGradeId);
+      if(grade){
+        rowData.gradeName = grade.name;
+      }
+      const gradeType = this.GradeClassifyList.find((item: any) => item.id == rowData.gradeTypeId);
+      if(gradeType){
+        rowData.typeName = gradeType.name;
+      }
+      const carNo = this.CarNo.find((item: any) => item.id == rowData.carNo);
+      if(carNo){
+        rowData.carNumber = carNo.name;
+      }
+      // const product = this.productsList.find((item: any) => item.id == rowData.productId);
+      // if (product) {
+      //   rowData.productName = product.sapcode;
+      //   // this.productsList.splice(this.productsList.indexOf(product), 1);
+      // }
+      // const grade = this.steelGradeList.find((item: any) => item.id === rowData.steelGradeId);
+      // if (grade) { rowData.gradeName = grade.name; }
+
+      this.logSheetList2.push(rowData);
     }
   }
 
   removeItem(index: any) {
     // console.log(index);
-    // const removeItem = this.logSheetList[index];
+    // const removeItem = this.logSheetList1[index];
     // var product = this.productAllList.find((item: any) => item.id == removeItem.productId);
     // if (product) {
     //   // console.log(this.productsList.indexOf(product));
     //   this.productsList.splice(0, 0, product)
     // }
-    this.logSheetList.splice(index, 1);
+    this.logSheetList1.splice(index, 1);
+  }
+
+  removeItem2(index: any) {
+    // console.log(index);
+    // const removeItem = this.logSheetList1[index];
+    // var product = this.productAllList.find((item: any) => item.id == removeItem.productId);
+    // if (product) {
+    //   // console.log(this.productsList.indexOf(product));
+    //   this.productsList.splice(0, 0, product)
+    // }
+    this.logSheetList2.splice(index, 1);
   }
 
 
@@ -137,7 +207,7 @@ export class LfLogsheetComponent implements OnInit{
   }
 
   validateSave() {
-    if (!this.logSheetList?.length) {
+    if (!this.logSheetList1?.length) {
       return false;
     }
     return true;
@@ -163,7 +233,7 @@ export class LfLogsheetComponent implements OnInit{
       carNo:0,
       lrfProcessTime:0
     }
-    this.logSheetList = [];
+    this.logSheetList1 = [];
     this.isLfLogSheetDialog = false;
     this.errorMessage = "";
   }
