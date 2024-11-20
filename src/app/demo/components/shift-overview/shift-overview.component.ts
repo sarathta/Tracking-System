1d8c2e4467eee318b5e9e06efc7c2fe50edb2cb8
sandrajayachandran12 <sandra@aicnet.in>
2024-11-20 10:09:24 +0530

    changes

--- src/app/demo/components/shift-overview/shift-overview.component.ts
+++ src/app/demo/components/shift-overview/shift-overview.component.ts
@@ -1,24 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
   selector: 'app-shift-overview',
   templateUrl: './shift-overview.component.html',
   styleUrls: ['./shift-overview.component.scss']
 })
-export class ShiftOverviewComponent {
+export class ShiftOverviewComponent implements OnInit{
 
   plantName: string = 'ABC'; // Example plant name
   currentDateTime: string = ''; // To hold current timestamp
   shiftOverview: string = ''; // Shift information
   private timer: any;
+  selectedData: any;
+  loading: boolean =false;
+ 
 
+  
+
+  constructor(
+    private layoutService: LayoutService,
+
+  ) { }
+
+  
   ngOnInit(): void {
+    this.layoutService.onMenuToggle();
     this.shiftOverview = this.getShiftDetails(); // Set shift details
     this.updateDateTime(); // Initialize date and time
     this.timer = setInterval(() => {
       this.updateDateTime(); // Update timestamp every second
     }, 1000);
   }
+
+  // constructor(private flashNewsService: FlashNewsService) {}
+
+  // ngOnInit1(): void {
+  //   this.news = this.flashNewsService.getNews();
+  //   this.updateNews();
+  // }
+
+  
+
 
   ngOnDestroy(): void {
     if (this.timer) {
